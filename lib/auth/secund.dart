74b97ad6792ad97f3919b55a325d37f48a9703d7
Mohammed Rabeeh P <rabeehm802@gmail.com>
2024-11-18 09:32:23 +0530

    bug fixes

---
+++ lib/auth/secund.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Secund extends StatelessWidget {
+  const Secund({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
